f6a40a6b162e15d1341709384d9fedd00251f5e8
Sebastian Alejos <bastialejos@hey.com>
2021-09-16 11:16:23 -0400

    Form exercise created

---
+++ Forms_PHP/process.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Welcome <?php echo $_POST["f_name"]; ?><br>
+Your email address is: <?php echo $_POST["user_id"]; ?></p>
+
+</body>
+</html>m
